55db513db7fb38e51cb314bb2b6840c9027bd9f0
iamarobot2 <avinjoshy21@gmail.com>
2023-03-31 14:04:25 +0530

    update

--- preloader.js
+++ preloader.js
@@ -1,14 +1,20 @@
 document.onreadystatechange = function preloader() {
     if (document.readyState !== "complete") {
+        let preloader = document.getElementById("loader");
         document.querySelector(
         "body").style.visibility = "hidden";
         document.querySelector(
         "#loader").style.visibility = "visible";
+        setTimeout(function() {
+            preloader.style.visibility = "hidden";
+            document.querySelector(
+                "#loader").style.display = "none";
+                document.querySelector(
+                "body").style.visibility = "visible";
+          }, 2000);
     } else {
-        document.querySelector(
-        "#loader").style.display = "none";
-        document.querySelector(
-        "body").style.visibility = "visible";
+        
     }
 };
+
     
