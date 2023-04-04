b06f647752faea31027ec5ea57098a739b95322c
iamarobot2 <avinjoshy21@gmail.com>
2023-04-04 17:02:59 +0530

    Mobile Navigation Added ,Error
    side navigation for small devices added but not working.
    display sublinks on the navbar on hover -Not working.
    Navbar is now rrsponsive

--- script/index.js
+++ script/index.js
@@ -1,10 +1,58 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+const navbar = document.getElementById('navbar');
+const sideNav = document.getElementById('sideNav');
+const sideNavButton = document.getElementById('sideNavButton');
+
+function toggleSideNav() {
+  sideNav.classList.toggle('active');
+}
+
+function closeSideNav() {
+  sideNav.classList.remove('active');
+}
+
+// Hide the navbar on small devices and show the side nav button
+function showSideNavButton() {
+  if (window.innerWidth < 768) {
+    sideNavButton.style.display = 'block';
+    navbar.style.display = 'none';
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    sideNavButton.style.display = 'none';
+    navbar.style.display = 'flex';
   }
-  prevScrollpos = currentScrollPos;
+}
+
+// Show the navbar when the window is resized to a larger size
+function showNavbar() {
+  if (window.innerWidth >= 768) {
+    navbar.style.display = 'flex';
+  } else {
+    navbar.style.display = 'none';
+  }
+}
+
+// Toggle the visibility of the dropdown menu on small devices
+const dropdownLinks = document.querySelectorAll('.dd');
+dropdownLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    link.querySelector('.list').classList.toggle('active');
+  });
+});
+
+window.addEventListener('resize', () => {
+  showNavbar();
+  showSideNavButton();
+});
+
+showNavbar();
+showSideNavButton();
+
+sideNavButton.addEventListener('click', toggleSideNav);
+sideNav.addEventListener('click', closeSideNav);
+
+function openNav() {
+  document.querySelector('.sidenav').style.width = '250px';
+}
+
+function closeNav() {
+  document.querySelector('.sidenav').style.width = '0';
 }
