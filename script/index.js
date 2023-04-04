dc88c3c21efab739d001f9fda31fe753a8ffc815
iamarobot2 <avinjoshy21@gmail.com>
2023-04-04 12:30:56 +0530

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,8 +1,3 @@
-window.onload = function() {
-  // Load the preloader page
-  window.location.href = "preloader.html";
-};
-
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
