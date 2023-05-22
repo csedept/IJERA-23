e16675bbd80d08580f4337eca5f4c0bc8610f288
Avin Joshy <avinjoshy21@gmail.com>
2023-05-22 13:38:06 +0530

    update

--- script/index.js
+++ script/index.js
@@ -60,16 +60,16 @@ for (var i = 0; i < sidenavLinks.length; i++) {
   sidenavLinks[i].addEventListener('click', toggleSubnav);
 }
 
-// Define the scroll position at which to add the "scrolled" class
-const scrollThreshold = 100;
-// Listen for the "scroll" event on the window object
-window.addEventListener('scroll', () => {
-  // Get the current scroll position
-  const scrollPosition = window.scrollY || window.pageYOffset;
-  // Add or remove the "scrolled" class based on the scroll position
-  if (scrollPosition >= scrollThreshold) {
-    navbar.classList.add('navbar-scrolled');
-  } else {
-    navbar.classList.remove('navbar-scrolled');
-  }
-});
+// // Define the scroll position at which to add the "scrolled" class
+// const scrollThreshold = 100;
+// // Listen for the "scroll" event on the window object
+// window.addEventListener('scroll', () => {
+//   // Get the current scroll position
+//   const scrollPosition = window.scrollY || window.pageYOffset;
+//   // Add or remove the "scrolled" class based on the scroll position
+//   if (scrollPosition >= scrollThreshold) {
+//     navbar.classList.add('navbar-scrolled');
+//   } else {
+//     navbar.classList.remove('navbar-scrolled');
+//   }
+// });
