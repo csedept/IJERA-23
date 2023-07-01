ff318e7d6b24c5da9d9c8dde5d5b26823ac40b2f
Avin Joshy <avinjoshy21@gmail.com>
2023-07-01 19:54:04 +0530

    update

--- script/archive.js
+++ script/archive.js
@@ -1,39 +1,298 @@
 var documents = [
     {
-      DOI: "",
-      Title: "",
-      Authors: [""],
-      InstituteName: [""],
-      DownloadLink: ""
+      DOI: "NACORE P103",
+      Title: "The Carbon footprint of Machine Learning Models",
+      Authors: ["Anirudh Das,","Avisikta Modak"],
+      InstituteName: ["Haldia Institute of Technology Haldia,India"],
+      DownloadLink: "archive/NACORE P 103.docx"
     },
     {
-      DOI: "",
-      Title: "",
-      Authors: [""],
-      InstituteName: [""],
-      DownloadLink: ""
+      DOI: "NACORE P107",
+      Title: "A survey on enhancement of motor vehicle safety features using smart sensors",
+      Authors: ["Roshan Mathew Thomas,","Krishna Prabha S,","Toney G Jolly,","Tony Raju Kuzhinjalil"],
+      InstituteName: ["St. Joseph’s College of Engineering and Technology, Palai, Kottayam, India"],
+      DownloadLink: "archive/NACORE P 107.pdf"
     },
     {
-      DOI: "",
-      Title: "",
+      DOI: "NACORE P108",
+      Title: "Fault Detection of Transmission Lines Using Unmanned Aerial Vehicle (UAV)",
       Authors: [""],
-      InstituteName: [""],
-      DownloadLink: ""
+      InstituteName: ["Mar Baselios Christian College of Engineering and Technology,Kuttikanam, India"],
+      DownloadLink: "archive/NACORE P 108.pdf"
     },
     {
-      DOI: "",
-      Title: "",
+      DOI: "NACORE P109",
+      Title: "See You",
       Authors: [""],
-      InstituteName: [""],
-      DownloadLink: ""
+      InstituteName: ["Christ College of Engineering,Thrissur, India"],
+      DownloadLink: "archive/NACORE P 109.pdf"
     },
     {
-      DOI: "",
-      Title: "",
+      DOI: "NACORE P110",
+      Title: "Smart Technical Interviewing System",
       Authors: [""],
-      InstituteName: [""],
-      DownloadLink: ""
+      InstituteName: ["Universal Engineering College, Kerala, India"],
+      DownloadLink: "archive/NACORE P 110.pdf"
     },
+    {
+      DOI: "NACORE P113",
+      Title: "Detection of Diabetic Retinopathy for IT sector using CNN and ResNet50 models",
+      Authors: [""],
+      InstituteName: [" Vignan’s institute of information technology,Visakhapatnam, India"],
+      DownloadLink:"archive/NACORE P 113.pdf"
+    },
+    {
+      DOI: "NACORE P114",
+      Title: "Cardamom Plant Disease Detection Using Robot",
+      Authors: [""],
+      InstituteName: ["Mar Baselios Christian College of Engineering and Technology,Kuttikanam, India"],
+      DownloadLink:"archive/NACORE P 114.pdf"
+    },
+    {
+      DOI: "NACORE P116",
+      Title: "Hand Gesture Based Home Automation",
+      Authors: [""],
+      InstituteName: ["Universal Engineering College, Kerala, India"],
+      DownloadLink: "archive/NACORE P 116.pdf"
+    },
+    {
+      DOI: "NACORE P117",
+      Title: "Wild Animal Intrusion Detection And Repellent System",
+      Authors: [""],
+      InstituteName: ["Universal Engineering College, Kerala, India"],
+      DownloadLink: "archive/NACORE P 117.pdf"
+    },
+    {
+      DOI: "NACORE P118",
+      Title: "A High Speed Low Power 10T SRAM with high Robustness",
+      Authors: [""],
+      InstituteName: ["College of Engineering Chengannur, Chengannur, India"],
+      DownloadLink: "archive/NACORE P 118.pdf"
+    },
+    {
+      DOI: "NACORE P119",
+      Title: "Lip reading and prediction system based on deep learning",
+      Authors: [""],
+      InstituteName: ["Universal Engineering College, Kerala, India"],
+      DownloadLink:"archive/NACORE P 119.pdf"
+    },
+    {
+      DOI: "NACORE P120",
+      Title: "Developing an Empathetic Interaction Model for Elderly in Pandemics",
+      Authors: [""],
+      InstituteName: ["College of Engineering Chengannur,IHRD, Kerala, India"],
+      DownloadLink: "archive/NACORE P 120.pdf"
+    },
+    {
+      DOI: "NACORE P122",
+      Title: "Auxilia: Assistive Learning Tool for Children with Down Syndrome",
+      Authors: [""],
+      InstituteName: ["St. Joseph’s College of Engineering and Technology, Palai, Kottayam, India "],
+      DownloadLink: "archive/NACORE P 122.pdf"
+    },
+    {
+      DOI: "NACORE P124",
+      Title: "DG CURE: Model For Detection of Dysgraphia",
+      Authors: [""],
+      InstituteName: ["Universal Engineering College, Kerala, India"],
+      DownloadLink: "archive/NACORE P 124.pdf"
+    },
+    {
+      DOI: "NACORE P125",
+      Title: "Sand Battery Technology: A Promising Solution for Renewable Energy Storage",
+      Authors: [""],
+      InstituteName: ["Mar Baselios Christian College of Engineering and Technology,Kuttikanam, India"],
+      DownloadLink: "archive/NACORE P 125.pdf"
+    },
+    {
+      DOI: "NACORE P127",
+      Title: "Fake Currency Detection Using Image Processing and Machine Learning",
+      Authors: [""],
+      InstituteName: ["Universal Engineering College, Kerala, India"],
+      DownloadLink: "archive/NACORE P 127.pdf"
+    },
+    {
+      DOI: "NACORE P128",
+      Title: "Brain Tumor Analysis And Detection Using Deep Learning Technologies",
+      Authors: [""],
+      InstituteName: ["Universal Engineering College, Kerala, India"],
+      DownloadLink: "archive/NACORE P 128.pdf"
+    },
+    {
+      DOI: "NACORE P130",
+      Title: "Crop Yield and Price Prediction",
+      Authors: [""],
+      InstituteName: ["Universal Engineering College, Kerala, India"],
+      DownloadLink:"archive/NACORE P 130.pdf"
+    },
+    {
+      DOI: "NACORE P131",
+      Title: "Revolutionizing Nutritional Management Through Food Scanning And Object Detection: A New Android Application For Adults",
+      Authors: [""],
+      InstituteName: ["College of Engineering Poonjar, Kottayam, India "],
+      DownloadLink: "archive/NACORE P 131.pdf"
+    },
+    {
+      DOI: "NACORE P132",
+      Title: "A Concise Review On E-Commerce Website For Visually Impaired",
+      Authors: [""],
+      InstituteName: ["College Of Engineering Kidangoor, Kottayam, India"],
+      DownloadLink: "archive/NACORE P 132.pdf"
+    },
+    {
+      DOI: "NACORE P133",
+      Title: "Reading Device for the Deaf and Blind in Real Time Speech: A Literature Survey",
+      Authors: [""],
+      InstituteName: ["College Of Engineering Kidangoor, Kottayam, India"],
+      DownloadLink: "archive/NACORE P 133.pdf"
+    },
+    {
+      DOI: "NACORE P134",
+      Title: "Unmasking Fraudulent Job Ads: A Critical Review of Machine Learning Techniques for Detecting Fake Jobs",
+      Authors: [""],
+      InstituteName: ["Assam down town University, Guwahati, India"],
+      DownloadLink: "archive/NACORE P 134.pdf"
+    },
+    {
+      DOI: "NACORE P135",
+      Title: "Review on SJCET ARENA",
+      Authors: [""],
+      InstituteName: ["St. Joseph’s College of Engineering and Technology, Palai, India"],
+      DownloadLink: "archive/NACORE P 135.pdf"
+    },
+    {
+      DOI: "NACORE P136",
+      Title: "AMIGO application",
+      Authors: [""],
+      InstituteName: ["St. Joseph’s College of Engineering and Technology, Palai, India"],
+      DownloadLink: "archive/NACORE P 136.pdf"
+    },
+    {
+      DOI: "NACORE P138",
+      Title: "Skin Disease Scanner",
+      Authors: [""],
+      InstituteName: ["Universal Engineering College, Kerala, India"],
+      DownloadLink: "archive/NACORE P 138.pdf"
+    },
+    {
+      DOI: "NACORE P140",
+      Title: "A Literature Review on IMAGE FORGERY DETECTION",
+      Authors: [""],
+      InstituteName: ["St. Joseph’s College of Engineering and Technology, Palai, India"],
+      DownloadLink: "archive/NACORE P 140.pdf"
+    },
+    {
+      DOI: "NACORE P142",
+      Title: "32-bit RISC Processor Using Vedic Mathematics",
+      Authors: [""],
+      InstituteName: ["Saintgits College of Engineering, Kottayam,Kerala"],
+      DownloadLink: "archive/NACORE P 142.pdf"
+    },
+    {
+      DOI: "NACORE P143",
+      Title: "SARA: A College Receptionist System",
+      Authors: [""],
+      InstituteName: ["Universal Engineering College, Kerala, India"],
+      DownloadLink: "archive/NACORE P 143.pdf"
+    },
+    {
+      DOI: "NACORE P144",
+      Title: "A Reliable Method for Detecting Brain Tumors in Magnetic Resonance Images Utilizing EfficientNet",
+      Authors: [""],
+      InstituteName: ["St. Joseph’s College of Engineering and Technology, Palai, India"],
+      DownloadLink: "archive/NACORE P 144.pdf"
+    },
+    {
+      DOI: "NACORE P145",
+      Title: "A Review on Comparison of VGG-16 and DenseNet algorithms for analysing brain tumor in MRI image ",
+      Authors: [""],
+      InstituteName: ["St. Joseph’s College of Engineering and Technology, Palai, India"],
+      DownloadLink: "archive/NACORE P 145.pdf"
+    },
+    {
+      DOI: "NACORE P147",
+      Title: "College Bus Security And Management System",
+      Authors: [""],
+      InstituteName: ["St. Joseph’s College of Engineering and Technology, Palai, India"],
+      DownloadLink: "archive/NACORE P 147.pdf"
+    },
+    {
+      DOI: "NACORE P148",
+      Title: "Malware Classification Framework Based On Deep Learning",
+      Authors: [""],
+      InstituteName: ["St. Joseph’s College of Engineering and Technology, Palai, India"],
+      DownloadLink: "archive/NACORE P 148.pdf"
+    },
+    {
+      DOI: "NACORE P149",
+      Title: "Smart Meter using Blockchain",
+      Authors: [""],
+      InstituteName: ["College Of Engineering Kidangoor, Kottayam, India"],
+      DownloadLink: "archive/NACORE P 149.pdf"
+    },
+    {
+      DOI: "NACORE P150",
+      Title: "Prediction Model of Heart Disease for a Clinical Decision Support System",
+      Authors: [""],
+      InstituteName: ["St. Joseph’s College of Engineering and Technology, Palai, India"],
+      DownloadLink: "archive/NACORE P 150.pdf"
+    },
+    {
+      DOI: "NACORE P152",
+      Title: "Exploring the Potential of Local Wi-Fi Networks for Offline Communication",
+      Authors: [""],
+      InstituteName: ["St. Joseph’s College of Engineering and Technology, Palai, India"],
+      DownloadLink: "archive/NACORE P 152.pdf"
+    },
+    {
+      DOI: "NACORE P153",
+      Title: "Number Plate Detection in Fog and Haze",
+      Authors: [""],
+      InstituteName: ["Universal Engineering College, Kerala, India"],
+      DownloadLink: "archive/NACORE P 153.pdf"
+    },
+    {
+      DOI: "NACORE P154",
+      Title: "Detection of Alzheimer's Disease and Assistance",
+      Authors: [""],
+      InstituteName: ["St. Joseph’s College of Engineering and Technology, Palai, India"],
+      DownloadLink: "archive/NACORE P 154.pdf"
+    },
+    {
+      DOI: "NACORE P155",
+      Title: "Interactive Toy",
+      Authors: [""],
+      InstituteName: ["Universal Engineering College, Kerala, India"],
+      DownloadLink: "archive/NACORE P 155.pdf"
+    },
+    // {
+    //   DOI: "",
+    //   Title: "",
+    //   Authors: [""],
+    //   InstituteName: [""],
+    //   DownloadLink: ""
+    // },
+    // {
+    //   DOI: "",
+    //   Title: "",
+    //   Authors: [""],
+    //   InstituteName: [""],
+    //   DownloadLink: ""
+    // },
+    // {
+    //   DOI: "",
+    //   Title: "",
+    //   Authors: [""],
+    //   InstituteName: [""],
+    //   DownloadLink: ""
+    // },
+    // {
+    //   DOI: "",
+    //   Title: "",
+    //   Authors: [""],
+    //   InstituteName: [""],
+    //   DownloadLink: ""
+    // },
   ];
   
   function populateTable() {
@@ -96,6 +355,25 @@ var documents = [
       }
     }
   }
-  
-  populateTable();
-  
+populateTable();
+
+window.onscroll = function() {
+  showScrollToTopButton();
+};
+
+function showScrollToTopButton() {
+  var button = document.getElementById("scrollToTopButton");
+  if (document.body.scrollTop > 600|| document.documentElement.scrollTop > 600) {
+    button.classList.add("show");
+  } else {
+    button.classList.remove("show");
+  }
+}
+
+function scrollToTop() {
+  var position = document.documentElement.scrollTop || document.body.scrollTop;
+  if (position > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, position - position / 8);
+  }
+}
