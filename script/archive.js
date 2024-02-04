a55bc22b1d4b503d1a831d07b2ea23e529791a49
iamarobot2 <avinjoshy21@gmail.com>
2024-02-04 13:41:33 +0530

    volume 3 , issue 2 added
    links and documents not provideed yet.

--- script/archive.js
+++ script/archive.js
@@ -664,6 +664,18 @@
     },
   ];*/
 
+  var acc = document.getElementsByClassName("accordion");
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
 function searchTable() {
   var input = document.getElementById("searchInput").value.toLowerCase();
   var cards = document.getElementsByClassName("card");
